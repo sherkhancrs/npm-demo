71e61e77d0331be3743b0100b9fbf44053ba253a
Sherkhan <sherkhancrs@gmail.com>
2019-10-19 19:07:06 +0600

    Our first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var _ = require('underscore');
+
+// Core module
+// file or folder require(./underscore) file underscore.js or underscore/index.js
+// node_modules
+
+var result = _.contains([1, 2, 3], 2);
+console.log(result);
